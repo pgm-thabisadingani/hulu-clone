6fbdc18b7da257179dfcc4ae7f7c858fe0900820
pgm-thabisadingani <71275598+pgm-thabisadingani@users.noreply.github.com>
2021-08-06 22:57:13 +0200

    initial

---
+++ components/Thumbnail.js
@@ -0,0 +1,29 @@
+import { ThumbUpIcon } from "@heroicons/react/outline";
+import Image from "next/image";
+import { forwardRef } from "react";
+
+
+
+const Thumbnail = forwardRef(({result}, ref ) => {
+  const BASE_URL ="https://image.tmdb.org/t/p/original/"
+  return (
+    <div ref={ref} className="group cursor-pointer p-2 transition duration-200 ease-in transform  sm:hover:scale-105 hover:z-5">
+      <Image 
+        layout="responsive"
+        src={`${BASE_URL}${result.poster_path || result.backdrop_path }` || `${BASE_URL}${results.poster_path}`} 
+        height={1060} 
+        width={1920}/>
+      <div className="p-2">
+        <p className="truncate max-w-md">{result.overview}</p>
+        <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease group-hover:font-bold">{result.title || result.original_name}</h2>
+        <p className="flex items-center opacity-0 group-hover:opacity-100">
+          {result.media_type && `${result.media_type} • `}{" "}
+          {result.release_date || result.first_air_ate}  •{" "}
+          <ThumbUpIcon className="h-5 mx-2"/> {result.vote_count}
+        </p>
+      </div>
+    </div>
+  )
+})
+
+export default Thumbnail
